ba8c6c9e8f5aa85eb8926d31dd4368d49e9bb182
tgp93 <thalesgprimo@gmail.com>
2023-01-31 23:18:46 -0300

    created tables and aluno logic

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -2,16 +2,40 @@ package me.dio.academia.digital.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private Aluno aluno;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
 
-  private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+    private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
 
-  private double peso;
+    @Column(name = "peso_atual")
+    private double peso;
 
-  private double altura;
-
+    @Column(name = "altura_atual")
+    private double altura;
 }
